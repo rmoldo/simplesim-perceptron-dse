ab78a84650bc96fd5db5afb3e5b316808fe6f66f
Radu Moldovan <radu.moldovan@ulbsibiu.ro>
2022-04-18 15:51:56 +0300

    Finish dse module

--- src/ga/configuration.py
+++ src/ga/configuration.py
@@ -3,12 +3,15 @@ import numpy as np
 
 class MSimConfiguration:
     def __init__(self):
+        self.perceptrons = 2 ** np.random.randint(1, 13)
+        self.history_length = np.random.randint(1, 33)
+        self.weight_bits = np.random.randint(1, 33)
+
         self.decode_width = 2 ** np.random.randint(1, 6)
         self.issue_width = 2 ** np.random.randint(1, 6)
         self.commit_width = 2 ** np.random.randint(1, 6)
 
         self.rob_size = 2 ** np.random.randint(5, 11)
-        self.rb_size = 2 ** np.random.randint(5, 11)
         self.iq_size = 2 ** np.random.randint(5, 11)
 
         self.ialu = np.random.randint(2, 9)
@@ -16,36 +19,39 @@ class MSimConfiguration:
         self.imult = np.random.randint(1, 9)
         self.fpmult = np.random.randint(1, 9)
 
+        x = np.random.randint(3, 8)
+        y = np.random.randint(3, 8)
+
         self.dl1_sets = 2 ** np.random.randint(1, 16)
-        self.dl1_block_size = 2 ** np.random.randint(3, 9)
+        self.dl1_block_size = 2**x
         self.dl1_assoc = 2 ** np.random.randint(0, 4)
         self.dl1_replacement_strat = np.random.choice(["l", "r", "f"])
 
         self.il1_sets = 2 ** np.random.randint(1, 16)
-        self.il1_block_size = 2 ** np.random.randint(3, 9)
+        self.il1_block_size = 2**y
         self.il1_assoc = 2 ** np.random.randint(0, 4)
         self.il1_replacement_strat = np.random.choice(["l", "r", "f"])
 
-        self.ul2_sets = 2 ** np.random.randint(8, 22)
-        self.ul2_block_size = 2 ** np.random.randint(6, 9)
+        self.ul2_sets = 2 ** np.random.randint(8, 19)
+        self.ul2_block_size = 2 ** np.random.randint(
+            max(x, y) + 1 if max(x, y) > 6 else 6, 9
+        )
         self.ul2_assoc = 2 ** np.random.randint(1, 5)
         self.ul2_replacement_strat = np.random.choice(["l", "r", "f"])
+
+        if self.ul2_sets * self.ul2_block_size < self.il1_sets * self.il1_block_size:
+            self.ul2_sets, self.il1_sets = self.il1_sets, self.ul2_sets
+        elif self.ul2_sets * self.ul2_block_size < self.dl1_sets * self.dl1_block_size:
+            self.ul2_sets, self.dl1_sets = self.dl1_sets, self.ul2_sets
 
         dl1_size = self.dl1_block_size * self.dl1_sets
         il1_size = self.il1_block_size * self.il1_sets
         ul2_size = self.ul2_block_size * self.ul2_sets
 
-        if dl1_size > ul2_size or il1_size > ul2_size:
-            if dl1_size > il1_size:
-                self.ul2_block_size, self.ul2_sets = self.dl1_block_size, self.dl1_sets
-            else:
-                self.ul2_block_size, self.ul2_sets = self.il1_block_size, self.il1_sets
-
     def get_clargs(self):
-        # TODO: add perceptron config when ready
         cache_dl1 = f"-cache:dl1 dl1:{self.dl1_sets}:{self.dl1_block_size}:{self.dl1_assoc}:{self.dl1_replacement_strat} "
         cache_il1 = f"-cache:il1 il1:{self.il1_sets}:{self.il1_block_size}:{self.il1_assoc}:{self.il1_replacement_strat} "
-        cache_ul2 = f"-cache:dl2 ul2:{self.ul2_sets}:{self.ul2_block_size}:{self.ul2_assoc}:{self.ul2_replacement_strat} "
-        other_config = f"-issue:width {self.issue_width} -commit:width {self.commit_width} -decode:width {self.decode_width} -rob:size {self.rob_size} -rb:size {self.rb_size} -iq:size {self.iq_size} -res:ialu {self.ialu} -res:imult {self.imult} -res:fpalu {self.fpalu} -res:fpmult {self.fpmult} "
+        cache_ul2 = f"-cache:il2 dl2 -cache:dl2 ul2:{self.ul2_sets}:{self.ul2_block_size}:{self.ul2_assoc}:{self.ul2_replacement_strat} "
+        other_config = f"-bpred perceptron -bpred:perceptron {self.perceptrons} {self.weight_bits} {self.history_length} -issue:width {self.issue_width} -commit:width {self.commit_width} -decode:width {self.decode_width} -rob:size {self.rob_size} -iq:size {self.iq_size} -res:ialu {self.ialu} -res:imult {self.imult} -res:fpalu {self.fpalu} -res:fpmult {self.fpmult} "
 
         return (other_config + cache_dl1 + cache_il1 + cache_ul2).split()
--- src/ga/problem.py
+++ src/ga/problem.py
@@ -6,23 +6,72 @@ import subprocess
 
 
 class MSimProblem(FloatProblem):
-    def __init__(self, benchmark):
+    def __init__(self, benchmark_path):
         super(MSimProblem, self).__init__()
 
         self.number_of_objectives = 2
         self.number_of_constraints = 0
-        self.number_of_variables = 4  # TODO: add real number of variables when ready
+        self.number_of_variables = 24
 
         self.obj_directions = [self.MINIMIZE, self.MINIMIZE]
         self.obj_labels = ["Energy", "CPI"]
 
-        self.benchmark = benchmark
+        self.benchmark_path = benchmark_path
         self.file_counter = 1
 
-        self.lower_bound = []
-        self.upper_bound = []
+        self.lower_bound = [
+            2,
+            1,
+            1,
+            2,
+            2,
+            2,
+            32,
+            32,
+            2,
+            2,
+            1,
+            1,
+            2,
+            8,
+            1,
+            0,
+            2,
+            8,
+            1,
+            0,
+            256,
+            64,
+            2,
+            0,
+        ]
 
-        pass
+        self.upper_bound = [
+            4096,
+            32,
+            32,
+            32,
+            32,
+            32,
+            1024,
+            1024,
+            8,
+            8,
+            8,
+            8,
+            32768,
+            256,
+            8,
+            2,
+            32768,
+            256,
+            8,
+            2,
+            2097152,
+            256,
+            16,
+            2,
+        ]
 
     def evaluate(self, solution: FloatSolution) -> FloatSolution:
         config = MSimConfiguration()
@@ -32,27 +81,43 @@ class MSimProblem(FloatProblem):
                 "./sim-outorder",
                 "-redir:sim",
                 f"results/sim_config_{self.file_counter}.res",
+                "-redir:prog",
+                "results/out.prg",
                 "-max:inst",
                 "1000000",
             ]
             + config.get_clargs()
-            + [self.benchmark]
+            + [self.benchmark_path]
         )
+
+        print(f"Running simulator for config {self.file_counter}")
 
         # Open sim-outorder process with args
         process = subprocess.run(args, check=False, stdout=subprocess.DEVNULL)
 
         # Get IPC and power from file
-        energy = self.__get_attribute_from_file("Total Power Consumption")
-        cpi = 1 / self.__get_attribute_from_file("THROUGHPUT IPC")
+        energy = self.__get_attribute_from_file(
+            f"results/sim_config_{self.file_counter}.res", "Total Power Consumption"
+        )
+
+        cpi = 1 / self.__get_attribute_from_file(
+            f"results/sim_config_{self.file_counter}.res", "THROUGHPUT IPC"
+        )
 
         solution.objectives[0] = energy
         solution.objectives[1] = cpi
 
+        print(f"Sim_{self.file_counter}: Energy: {energy} CPI: {cpi}")
+
+        self.file_counter += 1
+
         return solution
 
-    def __get_attribute_from_file(self, attribute):
-        pass
+    def __get_attribute_from_file(self, file, attribute):
+        with open(file) as fp:
+            for line in fp:
+                if line.startswith(attribute):
+                    return float(line.split(":")[1].strip())
 
     def get_name(self):
         return "MSim simple perceptron problem"
